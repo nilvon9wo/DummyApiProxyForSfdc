ee0d7f67f8e91c3845fee905ac555278f18c7a57
Brian Kessler <kessler.bm@gmail.com>
2024-04-11 17:55:04 +0200

    Removed unnecessary mocks.

--- force-app/main/default/lwc/dapDummyApiProxy/__test__/dapDummyApiProxy.test.js
+++ force-app/main/default/lwc/dapDummyApiProxy/__test__/dapDummyApiProxy.test.js
@@ -2,14 +2,8 @@ import { LightningElement } from 'lwc';
 import { createElement } from 'lwc';
 import DapDummyApiProxy from 'c/dapDummyApiProxy';
 
-jest.fn();
-
 const elementUnderTest = 'c-dap-dummy-api-proxy';
 describe(elementUnderTest, () => {
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-
     describe('element renders', () => {
       it('renders c-dap-user-list', () => {
         // Arrange
